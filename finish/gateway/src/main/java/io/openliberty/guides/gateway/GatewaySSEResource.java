85ec3e39f27c8362b7c018eb96fad60625d15a73
Tiago Ferreira <t.ferreiraibm.com@Tiagos-MacBook-Pro.local>
2020-08-05 16:30:07 -0400

    updating readme to new sys/inv model

--- finish/gateway/src/main/java/io/openliberty/guides/gateway/GatewaySSEResource.java
+++ finish/gateway/src/main/java/io/openliberty/guides/gateway/GatewaySSEResource.java
@@ -18,29 +18,50 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 @ApplicationScoped
+// tag::path2[]
 @Path("/sse")
+// end::path2[]
 public class GatewaySSEResource {
 
     private Logger logger = Logger.getLogger(GatewaySSEResource.class.getName());
 
+    // tag::sse[]
     private Sse sse;
+    // end::sse[]
+    // tag::broadcaster[]
     private SseBroadcaster broadcaster;
+    // end::broadcaster[]
 
+    // tag::subscribeToSystems[]
     @GET
+    // tag::path3[]
     @Path("/")
+    // end::path3[]
+    // tag::sseMimeType[]
     @Produces(MediaType.SERVER_SENT_EVENTS)
+    // end::sseMimeType[]
     public void subscribeToSystem(
+        // tag::sseEventSinkParam[]
         @Context SseEventSink sink,
-        @Context Sse sse) {
+        // end::sseEventSinkParam[]
+        // tag::sseParam[]
+        @Context Sse sse
+        // end::sseParam[]
+        ) {
 
         if (this.sse == null || this.broadcaster == null) { 
             this.sse = sse;
+            // tag::newBroadcaster
             this.broadcaster = sse.newBroadcaster();
+            // end::newBroadcaster[]
         }
         
-        this.broadcaster.register(sink);        
+        // tag::registerSink[]
+        this.broadcaster.register(sink);
+        // end::registerSink[]     
         logger.info("New sink registered to broadcaster.");
     }
+    // end::subscribeToSystems[]
 
     private void broadcastData(String name, Object data) {
         if (broadcaster != null) {
@@ -56,15 +77,23 @@ public class GatewaySSEResource {
         }
     }
 
+    // tag::getSystemLoadMessage[]
+    // tag::incoming1
     @Incoming("systemLoad")
+    // end::incoming1
     public void getSystemLoadMessage(SystemLoad sl)  {
         logger.info("Message received from systemLoadTopic. " + sl.toString());
         broadcastData("systemLoad", sl);
     }
+    // end::getSystemLoadMessage[]
 
+    // tag::getPropertyMessage[]
+    // tag::incoming2
     @Incoming("propertyMessage")
+    // tag::incoming2
     public void getPropertyMessage(PropertyMessage pm)  {
         logger.info("Message received from propertyMessageTopic. " + pm.toString());
         broadcastData("propertyMessage", pm);
     }
+    // end::getPropertyMessage[]
 }

cd0ed881ab85275ba10a931651170c6c58c2ef43
Griffin Hadfield <griffhadfield@gmail.com>
2020-10-20 16:07:54 -0400

    readme updates

--- finish/frontend/src/main/webapp/js/index.js
+++ finish/frontend/src/main/webapp/js/index.js
@@ -21,11 +21,13 @@ function systemLoadHandler(event) {
     var system = JSON.parse(event.data);
     // end::parse[]
     if(document.getElementById(system.hostname)){
-        document.getElementById(system.hostname).cells[1].innerHTML = system.loadAverage.toFixed(2);
+        document.getElementById(system.hostname).cells[1].innerHTML =
+                                        system.loadAverage.toFixed(2);
     }else{
         var tableRow = document.createElement('tr');
         tableRow.id = system.hostname;
-        tableRow.innerHTML = '<td>' + system.hostname + '</td><td>' + system.loadAverage.toFixed(2) + '</td>';
+        tableRow.innerHTML = '<td>' + system.hostname + '</td><td>'
+                             + system.loadAverage.toFixed(2) + '</td>';
         document.getElementById('sysPropertiesTableBody').appendChild(tableRow);
     }
 }

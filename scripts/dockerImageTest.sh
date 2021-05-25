4057f76e5032731bd4fa562ed7805ca9bf3d5ebe
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2021-05-25 13:55:30 -0400

    linter fix

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-while getopts t:d: flag;
-do
+while getopts t:d: flag; do
     case "${flag}" in
-        t) DATE="${OPTARG}";;
-        d) DRIVER="${OPTARG}";;
-        *) echo "Invalid option";;
+    t) DATE="${OPTARG}" ;;
+    d) DRIVER="${OPTARG}" ;;
+    *) echo "Invalid option" ;;
     esac
 done
 
 echo "Testing latest OpenLiberty Docker image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml frontend/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml frontend/pom.xml
 cat system/pom.xml frontend/pom.xml
 
 sed -i "s;FROM openliberty/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" system/Dockerfile frontend/Dockerfile

302d8303ee3e484eb83c0a84b341b3b161303aa5
Morgan Chang <shin19991207@gmail.com>
2023-12-01 17:16:14 -0500

    update bitnami/kafka container script

--- finish/scripts/startContainers.bat
+++ finish/scripts/startContainers.bat
@@ -5,24 +5,22 @@ set NETWORK=reactive-app
 docker network create %NETWORK%
 
 docker run -d ^
-  -e ALLOW_ANONYMOUS_LOGIN=yes ^
-  --network=%NETWORK% ^
-  --name=zookeeper ^
-  --rm ^
-  bitnami/zookeeper:3 
-
-start /b docker run -d ^
-  -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181 ^
-  -e ALLOW_PLAINTEXT_LISTENER=yes ^
-  -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 ^
-  --hostname=kafka ^
-  --network=%NETWORK% ^
-  --name=kafka ^
-  --rm ^
-  bitnami/kafka:2 
+    -e ALLOW_PLAINTEXT_LISTENER=yes ^
+    -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 ^
+    -e KAFKA_CFG_NODE_ID=0 ^
+    -e KAFKA_CFG_PROCESS_ROLES=controller,broker ^
+    -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092,CONTROLLER://:9093 ^
+    -e KAFKA_CFG_CONTROLLER_QUORUM_VOTERS=0@kafka:9093 ^
+    -e KAFKA_CFG_CONTROLLER_LISTENER_NAMES=CONTROLLER ^
+    --hostname=kafka ^
+    --network=%NETWORK% ^
+    --name=kafka ^
+    --rm ^
+    bitnami/kafka:latest
 
 start /b docker run -d ^
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=%KAFKA_SERVER% ^
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info ^
   --network=%NETWORK% ^
   --name=system1 ^
   --rm ^
@@ -31,6 +29,7 @@ start /b docker run -d ^
 
 start /b docker run -d ^
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=%KAFKA_SERVER% ^
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info ^
   --network=%NETWORK% ^
   --name=system2 ^
   --rm ^
@@ -39,6 +38,7 @@ start /b docker run -d ^
 
 start /b docker run -d ^
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=%KAFKA_SERVER% ^
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info ^
   --network=%NETWORK% ^
   --name=system3 ^
   --rm ^
@@ -47,6 +47,7 @@ start /b docker run -d ^
 start /b docker run -d ^
   -p 9084:9084 ^
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=%KAFKA_SERVER% ^
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info ^
   --network=%NETWORK% ^
   --name=bff ^
   --rm ^
@@ -55,6 +56,7 @@ start /b docker run -d ^
 start /b docker run -d ^
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=%KAFKA_SERVER% ^
   -p 9080:9080 ^
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info ^
   --network=%NETWORK% ^
   --name=frontend ^
   --rm ^
--- finish/scripts/startContainers.sh
+++ finish/scripts/startContainers.sh
@@ -6,27 +6,25 @@ NETWORK=reactive-app
 docker network create $NETWORK
 
 docker run -d \
-  -e ALLOW_ANONYMOUS_LOGIN=yes \
-  --network=$NETWORK \
-  --name=zookeeper \
-  --rm \
-  bitnami/zookeeper:3 &
-
-docker run -d \
-  -e KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181 \
-  -e ALLOW_PLAINTEXT_LISTENER=yes \
-  -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 \
-  --hostname=kafka \
-  --network=$NETWORK \
-  --name=kafka \
-  --rm \
-  bitnami/kafka:2 &
+    -e ALLOW_PLAINTEXT_LISTENER=yes \
+    -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 \
+    -e KAFKA_CFG_NODE_ID=0 \
+    -e KAFKA_CFG_PROCESS_ROLES=controller,broker \
+    -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092,CONTROLLER://:9093 \
+    -e KAFKA_CFG_CONTROLLER_QUORUM_VOTERS=0@kafka:9093 \
+    -e KAFKA_CFG_CONTROLLER_LISTENER_NAMES=CONTROLLER \
+    --hostname=kafka \
+    --network=$NETWORK \
+    --name=kafka \
+    --rm \
+    bitnami/kafka:latest &
   
 sleep 15
 
 docker run -d \
   -e UPDATE_INTERVAL=3 \
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=$KAFKA_SERVER \
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info \
   --network=$NETWORK \
   --name=system1 \
   --rm \
@@ -35,6 +33,7 @@ docker run -d \
   docker run -d \
   -e UPDATE_INTERVAL=2 \
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=$KAFKA_SERVER \
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info \
   --network=$NETWORK \
   --name=system2 \
   --rm \
@@ -43,6 +42,7 @@ docker run -d \
   docker run -d \
   -e UPDATE_INTERVAL=6 \
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=$KAFKA_SERVER \
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info \
   --network=$NETWORK \
   --name=system3 \
   --rm \
@@ -51,6 +51,7 @@ docker run -d \
 docker run -d \
   -p 9084:9084 \
   -e MP_MESSAGING_CONNECTOR_LIBERTY_KAFKA_BOOTSTRAP_SERVERS=$KAFKA_SERVER \
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info \
   --network=$NETWORK \
   --name=bff \
   --rm \
@@ -58,6 +59,7 @@ docker run -d \
 
 docker run -d \
   -p 9080:9080 \
+  -e WLP_LOGGING_CONSOLE_LOGLEVEL=info \
   --network=$NETWORK \
   --name=frontend \
   --rm \

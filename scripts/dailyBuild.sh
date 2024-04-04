ec0fbc07101e4662d370e30f3b4cd72e6b8e0c30
Gilbert Kwan <gkwan@ca.ibm.com>
2024-04-04 11:33:16 -0400

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -11,12 +11,13 @@ done
 
 echo "Testing daily OpenLiberty image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml frontend/pom.xml
-cat system/pom.xml frontend/pom.xml
+sed -i "\#</containerRunOpts>#a<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" system/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" bff/pom.xml frontend/pom.xml
+cat system/pom.xml bff/pom.xml frontend/pom.xml
 
-sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile frontend/Dockerfile
-sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile frontend/Dockerfile
-cat system/Dockerfile frontend/Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile bff/Dockerfile frontend/Dockerfile
+sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile bff/Dockerfile frontend/Dockerfile
+cat system/Dockerfile bff/Dockerfile frontend/Dockerfile
 
 docker pull -q "$DOCKER_USERNAME/olguides:$BUILD"
 

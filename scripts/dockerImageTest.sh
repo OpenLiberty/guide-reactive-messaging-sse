2e0fa6ab2213b4760a05b1162e4f7c6ab9709a63
Gilbert Kwan <gkwan@ca.ibm.com>
2024-04-24 10:50:39 -0400

    enable docker image test

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -13,10 +13,12 @@ sed -i "\#</containerRunOpts>#a<install><runtimeUrl>https://public.dhe.ibm.com/i
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" bff/pom.xml frontend/pom.xml
 cat system/pom.xml bff/pom.xml frontend/pom.xml
 
-sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM openliberty/daily:latest;g" system/Dockerfile bff/Dockerfile frontend/Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi;g" system/Dockerfile bff/Dockerfile frontend/Dockerfile
 sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile bff/Dockerfile frontend/Dockerfile
 cat system/Dockerfile bff/Dockerfile frontend/Dockerfile
 
-docker pull -q "openliberty/daily:latest"
+echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin cp.stg.icr.io
+sudo docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi"
+sudo echo "build level:"; docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi
 
-../scripts/testApp.sh
+sudo ../scripts/testApp.sh
